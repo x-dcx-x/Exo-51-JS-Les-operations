c43237f13e2128a58b930c27ed3717c360200d3e
x-dcx-x <112556623+x-dcx-x@users.noreply.github.com>
2023-01-15 20:06:55 +0100

    commit initial

---
+++ public/assets/js/app.js
@@ -0,0 +1,18 @@
+let result = 5+6;
+
+let five = 5;
+let six = 6;
+
+alert(result=five+six);
+
+
+let addition = result +2;
+alert(addition)
+
+ let soustraction = six-five;
+alert(soustraction)
+
+alert(result*addition);
+alert(result**soustraction);
+
+alert(five % 2)
